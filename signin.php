e49f9996970a8b7ede97a0c4a5f8d518067e798d
Balu <balu.swt@gmail.com>
2022-09-03 10:27:36 +0530

    sign in page added

--- signin.php
+++ signin.php
@@ -72,87 +72,9 @@
                                             <label class="uk-align-right"><a class="uk-link-reset" href="#">Forgot password?</a></label>
                                         </div>
 
-                                        <?php
-                                            require 'db_connection.php';
-
-                                            if(isset($_SESSION['login_id'])){
-                                                header('Location: home.php');
-                                                exit;
-                                            }
-
-                                            require 'google-api/vendor/autoload.php';
-
-                                            // Creating new google client instance
-                                            $client = new Google_Client();
-
-                                            // Enter your Client ID
-                                            $client->setClientId('18300631632-28c3cij3sma0l2ed8r4qlsk901kts475.apps.googleusercontent.com');
-                                            // Enter your Client Secrect
-                                            $client->setClientSecret('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');
-                                            // Enter the Redirect URL
-                                            $client->setRedirectUri('http://localhost:84/google_login/login.php');
-
-                                            // Adding those scopes which we want to get (email & profile Information)
-                                            $client->addScope("email");
-                                            $client->addScope("profile");
-
-
-                                            if(isset($_GET['code'])):
-
-                                                $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-
-                                                if(!isset($token["error"])){
-
-                                                    $client->setAccessToken($token['access_token']);
-
-                                                    // getting profile information
-                                                    $google_oauth = new Google_Service_Oauth2($client);
-                                                    $google_account_info = $google_oauth->userinfo->get();
-                                                
-                                                    // Storing data into database
-                                                    $id = mysqli_real_escape_string($db_connection, $google_account_info->id);
-                                                    $full_name = mysqli_real_escape_string($db_connection, trim($google_account_info->name));
-                                                    $email = mysqli_real_escape_string($db_connection, $google_account_info->email);
-                                                    $profile_pic = mysqli_real_escape_string($db_connection, $google_account_info->picture);
-
-                                                    // checking user already exists or not
-                                                    $get_user = mysqli_query($db_connection, "SELECT `google_id` FROM `users` WHERE `google_id`='$id'");
-                                                    if(mysqli_num_rows($get_user) > 0){
-
-                                                        $_SESSION['login_id'] = $id; 
-                                                        header('Location: home.php');
-                                                        exit;
-
-                                                    }
-                                                    else{
-
-                                                        // if user not exists we will insert the user
-                                                        $insert = mysqli_query($db_connection, "INSERT INTO `users`(`google_id`,`name`,`email`,`profile_image`) VALUES('$id','$full_name','$email','$profile_pic')");
-
-                                                        if($insert){
-                                                            $_SESSION['login_id'] = $id; 
-                                                            header('Location: home.php');
-                                                            exit;
-                                                        }
-                                                        else{
-                                                            echo "Sign up failed!(Something went wrong).";
-                                                        }
-
-                                                    }
-
-                                                }
-                                                else{
-                                                    header('Location: login.php');
-                                                    exit;
-                                                }
-                                                
-                                            else: 
-                                                // Google Login Url = $client->createAuthUrl(); 
-                                            ?>
-
                                         <div class="uk-margin-small uk-width-1-1">
-                                            <a href="<?php echo $client->createAuthUrl(); ?>" class="uk-button uk-width-1-1 uk-button-primary uk-border-rounded uk-float-left" type="submit" name="submit">Sign in</a>
-                                            <?php endif; ?>
+                                            <button class="uk-button uk-width-1-1 uk-button-primary uk-border-rounded uk-float-left" type="submit" name="submit">Sign in</a>
+                                            
                                         </div>
 
 
